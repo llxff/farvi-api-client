87ea98746a5ef3b4c0f01cedd62de997dd5adf6e
ssnikolay <ssnikolay@gmail.com>
2013-05-20 12:30:27 +0400

    deliver email

--- lib/farvi-api-client/order_information.rb
+++ lib/farvi-api-client/order_information.rb
@@ -5,6 +5,10 @@ module Farvi
         def update_order_information(order_number, options={})
           put("orders/#{order_number}", options)
         end
+
+        def deliver_has_not_payment_email(order_number, options={})
+          put("orders/#{order_number}/deliver_has_not_payment_email", options)
+        end
       end
     end
   end
